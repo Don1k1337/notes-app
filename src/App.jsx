5341a6ce5ab3361261612fbf7e3a28f2b4ff91df
Don1k1337 <dlyaspamnyhpisem@gmail.com>
2023-03-06 14:37:31 +0600

    fix: unnecessary lib removed

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,18 @@ import MainHeader from "./components/MainHeader.jsx";
 import NotesList from "./components/Notes/NotesList.jsx";
 import {useState} from "react";
 import {showModalHandler, hideModalHandler} from "./utils/modalUtils.js";
-import {QueryClient, QueryClientProvider} from "react-query";
-const queryClient = new QueryClient();
 function App() {
   const [modalIsVisible, setModalIsVisible] = useState(false);
   return (
       <>
-
         <MainHeader
             onCreateNote={showModalHandler(setModalIsVisible)}
         />
         <main>
-            <QueryClientProvider client={queryClient}>
-
           <NotesList
               isModalVisible={modalIsVisible}
               onStopNote={hideModalHandler(setModalIsVisible)}
           />
-          </QueryClientProvider>
-
         </main>
       </>
   );
