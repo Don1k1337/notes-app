91cc8a5fe5024921ba5de874ea6dd6ff79bfd788
Don1k1337 <dlyaspamnyhpisem@gmail.com>
2023-03-07 10:41:27 +0600

    feat: migrated from hooks to react-query for async req.

--- src/components/Notes/NotesList.jsx
+++ src/components/Notes/NotesList.jsx
@@ -2,78 +2,55 @@ import {Note} from "./Note.jsx";
 import AddNewNote from "./AddNewNote.jsx";
 import Modal from "../Modals/Modal.jsx";
 import classes from './NotesList.module.css';
-import {useState, useEffect} from "react";
 import instance from "../../axios/fetchData.js";
 import AppSpinner from "../Spinners/AppSpinner.jsx";
+import {useQuery} from 'react-query'
 
 function NotesList({isModalVisible, onStopNote}) {
-    const [notes, setNotes] = useState([]);
-    const [isLoading, setIsLoading] = useState(false)
-
-    useEffect(() => {
-        async function fetchData() {
-            setIsLoading(true);
-            try {
-                const res = await instance.get("/notes.json");
-                const fetchedData = [];
-
-                for (const key in res.data) {
-                    fetchedData.push({
-                        id: key,
-                        ...res.data[key],
-                    });
-                }
-                setNotes(fetchedData);
-            } catch (error) {
-                console.log(error);
-            }
-            setIsLoading(false);
-        }
-        fetchData().catch((error) =>
-            console.error("Unhandled promise rejection:", error)
-        );
-    }, []);
-
-    async function addNewNote(data) {
-        setIsLoading(true);
-
+    const { data: notes = [], isLoading, isError, refetch } = useQuery(
+        'notes',
+        fetchNotes,
+        {retry: 3})
+    async function fetchNotes() {
         try {
-            const res = await instance.post("/notes.json", data);
-            const newNote = {
-                ...data,
-                id: res.data.name,
-            };
-            setNotes((currentNotes) => [newNote, ...currentNotes]);
+            const res = await instance.get('/notes.json');
+            const fetchedData = [];
+            for (const key in res.data) {
+                fetchedData.push({id: key, ...res.data[key]});
+            }
+            return fetchedData;
         } catch (e) {
+            console.error('Failed to load the data from server response')
+            throw e
+        }
+    }
+    async function addNewNote(data) {
+        try {
+            await instance.post('/notes.json', data);
+            await refetch()
+        } catch(e) {
             console.error("Error caused during POST", e);
         }
-
-        setIsLoading(false);
     }
-
-
     return (
         <>
             {isModalVisible && (
                 <Modal onHideModal={onStopNote}>
                     <AddNewNote onCancel={onStopNote} onAddNote={addNewNote} />
                 </Modal>
-            )}{!isLoading && notes.length > 0 && (
+            )}
             <ul className={classes.notes}>
                 {notes.map((note) => {
                     return <Note key={`note-${note.id}`} author={note.author} body={note.body} />;
-
                 })}
             </ul>
-        )}
-            {!isLoading && notes.length === 0 && (
+            {isError && (
                 <div className={classes.empty}>
-                    <h2>No notes have been added yet.</h2>
-                    <p>Click on a button to add one.</p>
+                    <p>Seems to be error caused on server-side</p>
+                    <button onClick={refetch}>Retry</button>
                 </div>
             )}
-            {isLoading && (<AppSpinner />
-            )}
+            {isLoading && <AppSpinner />}
         </>
     );
 }
